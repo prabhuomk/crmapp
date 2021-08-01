1db549b56aa1b568c5816e8d7cccf20ea7750d1d
prabhuomk <prabhuomk@gmail.com>
2021-08-01 19:19:17 +0530

    connected

---
+++ helper.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+export async function insertUser(client, user) {
+    const result = await client.db("crm").collection("user").insertOne(user);
+    console.log("successfully pass inserted", result);
+    return result;
+}
+
+export async function getUsers(client, filter) {
+    const result = await client.db("crm").collection("user").find(filter).toArray();
+    console.log("successfully found the user", result);
+    return result;
+}
+
+
+export async function getUser(client, filter) {
+    const result = await client.db("crm").collection("user").findOne(filter);
+    console.log("successfully matched", result);
+    return result;
+}
+
+export async function updatePollById(client, id,newPoll) {
+    const result = await client.db("contestant").collection("poll").updateOne({ id: id },{$set:newPoll});
+    console.log("successfully updated", result);
+    return result;
+}
+
+
---
+++ routes/user.js
@@ -0,0 +1,70 @@
+import {  insertUser,getUsers ,getUser} from "../helper.js";
+
+import {createConnection} from "../index.js";
+import express from 'express';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {auth} from "../middleware/auth.js"
+
+
+const router=express.Router();
+
+
+
+
+router
+.route("/signup")
+.post(async (request,response)=>{
+    const { username,firstname,lastname,password }= request.body;
+    
+    const indbstoredusername=user.username;
+
+    const client=await createConnection();
+    const hashedPassword=await genPassword(password);
+
+    const pass=await insertUser(client,{username:username,firstname:firstname,lastname:lastname,password:hashedPassword})
+    console.log(hashedPassword,pass );
+    response.send(pass);
+    
+});
+
+router
+.route("/login")
+.post(async (request,response)=>{
+    const { username,password }= request.body;
+    const client=await createConnection();
+    const user=await getUser(client,{username:username});
+    console.log(user);
+    const inDbStoredPassword=user.password;
+    const isMatch= await bcrypt.compare(password,inDbStoredPassword);
+    if(isMatch){
+        const token=jwt.sign({id:user._id},process.env.KEY)
+        response.send({message:"successfully login",token:token});
+    }
+    else{
+        response.send({message:"invalid login"});
+
+    }  
+    
+});
+
+
+router
+.route("/")
+.get(auth, async (request,response)=>{
+    const client=await createConnection();
+    const contestants=await getUsers(client,{});
+    response.send(contestants);
+});
+
+async function genPassword(password){
+    
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    return hashedPassword;
+}
+
+
+
+
+export const userRouter=router;
