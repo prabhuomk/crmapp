971a5b6da0f1c7bb3b2f42edb31396ef0cbec2e6
prabhuomk <prabhuomk@gmail.com>
2021-09-02 11:13:37 +0530

    sm

--- routes/user.js
+++ routes/user.js
@@ -42,7 +42,7 @@ router
     const isMatch= await bcrypt.compare(password,inDbStoredPassword);
     if(isMatch){
         const token=jwt.sign({id:user._id},process.env.KEY)
-        localStorage.setItem('token',token);
+        response.localStorage.setItem('token',token);
         response.send({message:"successfully login",token:token});
     }
     else{
